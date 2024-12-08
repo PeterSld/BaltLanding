77bc608b2aa300fdf3cd7f2a7902225d60a402e5
PeterSld <127738070+PeterSld@users.noreply.github.com>
2024-12-08 20:16:15 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,16 @@
 AOS.init({
     easing: 'ease-in-quad',
 })
+
+var modal = document.getElementById("modal");
+var button_modal = document.getElementById("button_modal")
+button_modal.onclick = function() {
+    modal.style.display = "flex";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
